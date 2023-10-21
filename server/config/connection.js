94c2a21881bdea2887bafef1920b317c2d389afe
unknown <samtisme@gmail.com>
2023-10-20 21:10:42 -0700

    created mongoose connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,8 @@
 // create mongoose connection to mongoDB
-
 const mongoose = require('mongoose');
 
-mongoose.set('debug', true);
+// connect to database by wrapping mongoose.connect() in a promise
+mongoose.connect('mongodb://127.0.0.1:27017/odyssey');
 
+// export mongoose connection
 module.exports = mongoose.connection;
