a13f3a5efd6ba4d3efce1f627d48299eb9e912a8
unknown <samtisme@gmail.com>
2023-10-23 01:29:22 -0700

    added barrel file for controllers for easier require statements

---
+++ server/controllers/index.js
@@ -0,0 +1,21 @@
+// barrel file for all controllers 
+
+const userController = require('./user-cntrl');
+const stockController = require('./stock-cntrl');
+const userstockController = require('./userstock-cntrl');
+const eventController = require('./event-cntrl');
+const newsController = require('./news-cntrl');
+const marketController = require('./market-cntrl');
+const filterController = require('./filter-cntrl');
+
+// export all controllers as properties on an object
+
+module.exports = {
+    userController,
+    stockController,
+    userstockController,
+    eventController,
+    newsController,
+    marketController,
+    filterController
+};
